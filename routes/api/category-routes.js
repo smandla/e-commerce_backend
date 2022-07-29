a1fb5943e1df1eec9353c659562bd511211dc9ab
smandla <35283559+smandla@users.noreply.github.com>
2022-07-29 13:21:57 -0700

    delete category

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -46,10 +46,28 @@ router.post("/", async (req, res) => {
 
 router.put("/:id", (req, res) => {
   // update a category by its `id` value
+  // try {
+  //   const categoryData = await Category.update({})
+  // } catch (error) {
+  // }
 });
 
-router.delete("/:id", (req, res) => {
+router.delete("/:id", async (req, res) => {
   // delete a category by its `id` value
+  console.log(req.params.id);
+  try {
+    const categoryData = await Category.destroy({
+      where: { id: req.params.id },
+    });
+    console.log(categoryData);
+    if (!categoryData) {
+      res.status(404).json({ message: "No category found with that id!" });
+      return;
+    }
+    res.status(200).json(`Deleted ${categoryData} Successfully!`);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
 module.exports = router;
